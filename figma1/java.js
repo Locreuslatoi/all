cf6882a14d8df0a69eebea57cc9dbbda5031f57e
Locreuslatoi <locd0262@gmail.com>
2023-02-21 16:56:19 +0700

    update_1

--- figma1/java.js
+++ figma1/java.js
@@ -1,15 +1,1 @@
-var state = true; // The starting state of the image - false means it's hidden
-document
-  .querySelector("#changeImageButton")
-  .addEventListener("click", function () {
-    state = !state; // Toggle between true and false each time the button is pressed
-    if (state) {
-      document.querySelector(".background").style.animation =
-        "myAnimation 10s infinite alternate ease";
-      document.querySelector(".background").style.webkitAnimation =
-        "myAnimation 10s infinite alternate ease";
-    } else {
-      document.querySelector(".background").style.animation = "";
-      document.querySelector(".background").style.webkitAnimation = "";
-    }
-  });
+<script>{}</script>;
